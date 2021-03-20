ac7f1bf358ae0c14332c980ab19501ae4b82a282
Arun Bahl <pitastrudl@gmail.com>
2021-03-20 21:46:33 +0100

    added denylist for words

--- randomWiki.py
+++ randomWiki.py
@@ -1,37 +1,62 @@
-import httplib2
-from bs4 import BeautifulSoup, SoupStrainer
-import re,random
+import requests
+from bs4 import BeautifulSoup
+import re,random, telegram,time 
 
-http = httplib2.Http()
-# status, response = http.request("https://en.wikipedia.org/wiki/Pinot_noir_passing-off_controversy")
+not_wanted_words = ["ISBN","ISO","(identifier)"]
 
-# for link in BeautifulSoup(response, parse_only=SoupStrainer('a'),features="html.parser"):
-#         if link.has_attr('href') and re.search("^/wiki/",link['href']):
-#             print(link['href'])
+def doesContain(link,words):
+    match=link.split("https://en.wikipedia.org/wiki/",1)[1]
+    for w in words:
+        if match.__contains__(w):
+        # if match.startswith(w):
+            print("it's matching: ",match)
+            return True
+    return False
 
-# soup = BeautifulSoup(response, parse_only=SoupStrainer('a'),features="html.parser")     
-# res= soup.findAll(attrs={'href': re.compile(r"^/wiki/")})
-
-f = open("lastlink.txt", "r")
-testlink=f.read()
-
-print("The first link is: " + testlink)
 def returnrandompage(page):
-    #page argument is the string
-    status, response = http.request(page)
-    soup = BeautifulSoup(response,features="html.parser") 
-    content=soup.find("div",{"class": "mw-parser-output"})    
-    res = content.findAll(attrs={'href': re.compile(r"^/wiki/[^:]+$")})
-    print(len(res))
-    randchoice=random.choice(res) 
-    return randchoice['href']
+    r = requests.get(page)
+    soup = BeautifulSoup(r.content, features="html.parser")
+    mainContent = soup.find(id='mw-content-text')
+    links = mainContent.findAll(attrs={'href': re.compile(r"^/wiki/[^:]+$")})
+    res = random.choice(links)['href']
+    return(res)
 
-nextlink=testlink
-for x in range(5):
+# Bot
+bot_token = ""
+bot_chatID_julie = ""
+bot_chatID_arun = ""
+
+bot = telegram.Bot(token=bot_token)
+
+
+
+# Get the first link
+f = open("lastlink.txt", "r")
+nextlink=f.read()
+print("The first link is: " + nextlink)
+
+# Find the next 3 random links
+for x in range(3):
     nextlink = returnrandompage(nextlink)
-    nextlink = "https://en.wikipedia.org/" + nextlink
+    #nextlink ="/wiki/ISBN_(identifier)"
+    nextlink = "https://en.wikipedia.org" + nextlink
     print(nextlink)
+    while doesContain(nextlink,not_wanted_words):
+        nextlink = returnrandompage(nextlink)
+        # nextlink ="/wiki/ISBN_(identifier)"
+        nextlink = "https://en.wikipedia.org" + nextlink
+        print("im loopin, found unwanted link: ",nextlink)
+    # print("ive finished while loop or sent message, beurk")
+    bot.sendMessage(chat_id=bot_chatID_arun, text=nextlink)  
+    bot.sendMessage(chat_id=bot_chatID_julie, text=nextlink)
+    #sendtext = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&text=' + nextlink
+    #requests.get(sendtext)
+    time.sleep(1)
 
-f = open("lastlink.txt", "w")
+
+# Save the last link
+f = open("lastlink.txt", "w") 
 f.write(nextlink)
 f.close()
+
+#https://api.telegram.org/bot<bottoken>/getUpdates
